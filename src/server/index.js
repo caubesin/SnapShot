2642543ac961c623085d425066a5bacdeb945fa7
caubesin <60962992+caubesin@users.noreply.github.com>
2021-04-27 22:05:03 +0700

    Config img

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,7 @@ import {apiKey} from '../const/apiKey'
 const getData = (queryString, page) => {
     return new Promise((resolve, reject) => {
         axios
-            .get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${queryString}&page=${page}&per_page=25&format=json&nojsoncallback=1`)
+            .get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${queryString}&page=${page}&per_page=24&format=json&nojsoncallback=1`)
             .then((response) =>{
                 resolve(response.data.photos.photo);
             })
